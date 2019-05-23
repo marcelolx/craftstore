9904a2eea4069d5b7de38784172dc64b24454f54
Marcelo Lauxen <marcelolauxen16@gmail.com>
2019-05-23 23:11:57 +0000

    Gera controllers e view do devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
-  devise_for :usuarios
+  devise_for :usuarios, controllers: {
+    sessions: 'usuarios/sessions'
+  }
   #devise_for :users
   #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
